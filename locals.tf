c857536a95ca4a48199d874217ebe435fdbae806
zoki <tosevzoran@gmail.com>
2024-11-23 22:26:06 +0100

    fix: terraform product service repo name

--- locals.tf
+++ locals.tf
@@ -1,51 +1,51 @@
 locals {
   repos = {
     "fem-terraform-eci-tfe" = {
-      description = "Automation for Terraform Enterprise"
+      description        = "Automation for Terraform Enterprise"
       gitignore_template = "Terraform"
-      name = "fem-terraform-eci-tfe"
-      topics = ["frontend-masters", "terraform", "devops-tutorials"]
-      visibility = "public"
+      name               = "fem-terraform-eci-tfe"
+      topics             = ["frontend-masters", "terraform", "devops-tutorials"]
+      visibility         = "public"
     }
 
     "fem-terraform-eci-github" = {
-      description = "Automation for GitHub"
+      description        = "Automation for GitHub"
       gitignore_template = "Terraform"
-      name = "fem-terraform-eci-github"
-      topics = ["frontend-masters", "terraform", "devops-tutorials"]
-      visibility = "public"
+      name               = "fem-terraform-eci-github"
+      topics             = ["frontend-masters", "terraform", "devops-tutorials"]
+      visibility         = "public"
     }
 
     "fem-eci-terraform-aws-network" = {
-      description = "Automation for GitHub"
+      description        = "Automation for GitHub"
       gitignore_template = "Terraform"
-      name = "fem-terraform-eci-aws-network"
-      topics = ["frontend-masters", "terraform", "devops-tutorials"]
-      visibility = "public"
+      name               = "fem-terraform-eci-aws-network"
+      topics             = ["frontend-masters", "terraform", "devops-tutorials"]
+      visibility         = "public"
     }
 
     "fem-eci-terraform-aws-cluster" = {
-      description = "Automation for GitHub"
+      description        = "Automation for GitHub"
       gitignore_template = "Terraform"
-      name = "fem-terraform-eci-aws-cluster"
-      topics = ["frontend-masters", "terraform", "devops-tutorials"]
-      visibility = "public"
+      name               = "fem-terraform-eci-aws-cluster"
+      topics             = ["frontend-masters", "terraform", "devops-tutorials"]
+      visibility         = "public"
     }
 
     "fem-eci-terraform-product-service" = {
-      description = "Automation for product service"
+      description        = "Automation for product service"
       gitignore_template = "Go"
-      name = "fem-eci-service"
-      topics = ["frontend-masters", "terraform", "devops-tutorials"]
-      visibility = "public"
+      name               = "fem-eci-terraform-product-service"
+      topics             = ["frontend-masters", "terraform", "devops-tutorials"]
+      visibility         = "public"
     }
 
     "fem-eci-service" = {
-      description = "Example product servicw"
+      description        = "Example product servicw"
       gitignore_template = "Go"
-      name = "fem-eci-service"
-      topics = ["frontend-masters", "terraform", "devops-tutorials"]
-      visibility = "public"
+      name               = "fem-eci-service"
+      topics             = ["frontend-masters", "terraform", "devops-tutorials"]
+      visibility         = "public"
     }
   }
 }
